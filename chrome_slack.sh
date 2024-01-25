3ffb2eae21304308731fae3d94b688ba1cef847b
SomeoneWithOptions <andrescastesol@gmail.com>
2024-01-24 19:51:22 -0500

    made homebrew installation unnatended

--- chrome_slack.sh
+++ chrome_slack.sh
@@ -2,7 +2,7 @@
 
 if ! command -v brew &> /dev/null
 then
-    /bin/bash -c "$ (curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
     source ~/.zshrc
 fi
