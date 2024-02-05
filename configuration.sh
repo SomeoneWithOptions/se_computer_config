09f4fc6bbcfcef09552a8387703d00e9bdb30e4f
SomeoneWithOptions <andrescastesol@gmail.com>
2024-02-05 10:07:56 -0500

    updated full script

--- configuration.sh
+++ configuration.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
+LoggedInUserHome="/Users/$loggedInUser"
+UserPlist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
+
+echo using $loggedInUser to run commands
+
 # APP INSTALLATION
 if ! command -v brew &> /dev/null
 then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
-    source ~/.zshrc
+    sudo -u $loggedInUser bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    sudo -u $loggedInUser echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
+    sudo -u $loggedInUser source ~/.zshrc
 fi
 
-brew install bash
-brew install --cask google-chrome
-brew install --cask slack
-brew install --cask microsoft-office
-brew install dockutil 
+sudo -u $loggedInUser brew install bash
+sudo -u $loggedInUser brew install --cask google-chrome
+sudo -u $loggedInUser brew install --cask slack
+sudo -u $loggedInUser brew install --cask microsoft-office
+sudo -u $loggedInUser brew install dockutil 
 
 curl https://installers-stellar.s3.us-east-2.amazonaws.com/Endpoint.dmg --output ~/Downloads/bitdefender.dmg
 
@@ -31,7 +37,7 @@ sudo scutil --set HostName $name
 sudo scutil --set LocalHostName $name 
 sudo scutil --set ComputerName $name
 
-echo "Hostname Changed" 
+echo "Hostname Changed to $name" 
 
 # END CHANGE HOSTNAME
 
@@ -45,19 +51,18 @@ fi
 
 sudo sysadminctl -addUser p202admin -fullName "P202 Admin" -password p202wmsms! -admin
 
+echo "Admin User Created"
+
 # END CREATE ADMIN USER
 
 # DOCK CONFIGURATION 
 
-loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
-LoggedInUserHome="/Users/$loggedInUser"
-UserPlist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
-
-sudo -u $loggedInUser dockutil --remove all --no-restart $UserPlist
 sudo -u $loggedInUser dockutil --add "/Applications/Google Chrome.app" --no-restart $UserPlist
 sudo -u $loggedInUser dockutil --add "/Applications/Slack.app/" --no-restart $UserPlist
 sudo -u $loggedInUser dockutil --add /System/Applications/System\ Settings.app --position end --no-restart $UserPlist
 sudo -u $loggedInUser killall Dock
+
+echo "Dock Configuration Finished"
 
 # END DOCK CONFIGURATION
 
