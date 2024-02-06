8a6efc7822d1c52e9200d8781b4422cfde903024
SomeoneWithOptions <andrescastesol@gmail.com>
2024-02-06 15:05:22 -0500

    removed already declared variable loggedInUser and not need echo

--- configuration.sh
+++ configuration.sh
@@ -72,10 +72,7 @@ echo "Dock Configuration Finished"
 
 #ENABLE FIREVAULT
 
-loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
-
 fstatus=$(fdesetup status | grep -i "on")
-echo $fstatus
 
 if [[ -n $fstatus ]]; then
     echo "Firevault is Already ON"
